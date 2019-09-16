1e0812083299c83dd5e82621ff4f812f911953a0
Gadnalf <philip.cai.mao@hotmail.ca>
2019-09-16 17:54:34 -0400

    Finished circle intersection calculation

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
-public class GunControl : MonoBehaviour
+public class Dist : MonoBehaviour
 {
     public Vector3 mousePos { get; private set; }
     public Camera mainCamera;
@@ -24,12 +24,12 @@ public class GunControl : MonoBehaviour
 	public void Update()
 	{
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 elbow = new Vector3(rightShoulder.x, ) 
-        rightShoulder.Rotate(new Vector3(0, 0, 2f));
-        leftShoulder.Rotate(new Vector3(0, 0, 2f));
+        Vector3 rotate = new Vector3(0,0,3);
+        rightShoulder.Rotate(rotate);
+        leftShoulder.Rotate(rotate);
     }
 
-    private double Dist(Vector3 a, Vector3 b)
+    private double Dist2D(Vector3 a, Vector3 b)
     {
         return Math.Sqrt(Math.Pow(a.x - b.x, 2) * Math.Pow(a.y - b.y, 2));
     }
@@ -38,6 +38,44 @@ public class GunControl : MonoBehaviour
     {
 
         return 0;
+    }
+
+    private int FindIntersectOfCircles(Vector3 center1, double radius1, Vector3 center2, double radius2, out Vector3 intersect1, out Vector3 intersect2)
+    {
+        double dist = Dist2D(center1, center2);
+
+        if (dist > radius1 + radius2 | dist < Math.Abs(radius1 - radius2) | dist == 0) // There's some fuckery afoot
+        {
+            intersect1 = center1;
+            intersect2 = center1;
+            return 0;
+        }
+        else if (dist == radius1 + radius2) // One intersect
+        {
+            Vector3 direction = (center1 - center2).normalized;
+            direction.Scale(new Vector3((float)radius1, (float)radius1, 0));
+            intersect1 = center1 + direction;
+            intersect2 = center1;
+            return 1;
+        }
+        else
+        {
+            double a = (Math.Pow(radius1, 2) - Math.Pow(radius2, 2) + Math.Pow(dist, 2)) / (2 * dist);
+            double h = Math.Sqrt(Math.Pow(radius1, 2) - Math.Pow(a, 2));
+
+            Vector3 direction = (center1 - center2).normalized;
+            direction.Scale(new Vector3((float) a, (float) a, 0));
+
+            Vector3 vert = (center1 - center2).normalized;
+            Vector3 avert = vert;
+            vert.Scale(new Vector3((float) h, (float) -h, 0));
+            avert.Scale(new Vector3((float) -h,(float) h, 0));
+            
+            intersect1 = center1 + direction + vert;
+            intersect2 = center1 + direction + avert;
+            return 2;
+        }
+        
     }
 
     //private void FindTarget(Transform shoulder, Transform point, Vector3 target)
