8ae45530143c296eb6c5e778a324dec94bea6a18
Gadnalf <philip.cai.mao@hotmail.ca>
2019-09-17 17:36:39 -0400

    Unfucked the circle creation

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -10,6 +10,7 @@ public class GunControl : MonoBehaviour
     Transform rightShoulder;
     Transform leftPoint;
     Transform rightPoint;
+    Transform leftElbow;
 
     public void Start()
     {
@@ -17,12 +18,15 @@ public class GunControl : MonoBehaviour
         rightPoint = rightShoulder.Find("Hand");
         leftShoulder = transform.Find("Left Shoulder");
         leftPoint = leftShoulder.Find("Muzzle");
+        leftElbow = leftShoulder.Find("Elbow");
     }
 
     public void Update()
     {
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        double aimAngle = FindAimAngle(Vector3.zero, new Vector3(0, leftPoint.position.y, 0), mousePos);
+        Debug.Log(mousePos.ToString());
+        double aimAngle = FindAimAngle(leftShoulder.position, leftElbow.position, mousePos);
+        Debug.Log(aimAngle);
         Vector3 rotate = new Vector3(0, 0, (float)aimAngle);
         rightShoulder.Rotate(rotate);
         leftShoulder.Rotate(rotate);
@@ -36,12 +40,16 @@ public class GunControl : MonoBehaviour
     private double FindAimAngle(Vector3 pivot, Vector3 elbow, Vector3 target)
     {
         Vector3 intersect1, intersect2;
+        Vector3 mid = new Vector3((pivot.x + target.x)/2, (pivot.y + target.y) / 2, 0);
+        Debug.Log("full distance: " + Dist2D(pivot, target));
+        Debug.Log("mid distance: " + Dist2D(pivot, mid));
+        int numIntersects = FindIntersectOfCircles(pivot, Dist2D(pivot, elbow), mid, Dist2D(pivot, mid), out intersect1, out intersect2);
         double lengthToMidpoint;
-        if (FindIntersectOfCircles(pivot, Dist2D(pivot, elbow), target, Dist2D(pivot, target) / 2, out intersect1, out intersect2) == 2)
+        if (numIntersects == 2)
         {
             lengthToMidpoint = Math.Min(Dist2D(elbow, intersect1), Dist2D(elbow, intersect2)) / 2;
         }
-        else if (FindIntersectOfCircles(pivot, Dist2D(pivot, elbow), target, Dist2D(pivot, target) / 2, out intersect1, out intersect2) == 1)
+        else if (numIntersects == 1)
         {
             lengthToMidpoint = Dist2D(elbow, intersect1) / 2;
         }
@@ -58,14 +66,17 @@ public class GunControl : MonoBehaviour
 
         if (dist > radius1 + radius2 | dist < Math.Abs(radius1 - radius2) | dist == 0) // There's some fuckery afoot
         {
+            Debug.Log("there's fuckery afoot");
+            Debug.Log("too far?" + (dist > radius1 + radius2));
+            Debug.Log("one circle in the other somehow?" + (dist < Math.Abs(radius1 - radius2)));
+            Debug.Log("on point?" + (dist == 0));
             intersect1 = center1;
             intersect2 = center1;
             return 0;
         }
         else if (dist == radius1 + radius2) // One intersect
         {
-            Vector3 direction = (center1 - center2).normalized;
-            direction.Scale(new Vector3((float)radius1, (float)radius1, 0));
+            Vector3 direction = new Vector3((center1 - center2).normalized.x * (float) radius1, (center1 - center2).normalized.y * (float) radius1, 0);
             intersect1 = center1 + direction;
             intersect2 = center1;
             return 1;
