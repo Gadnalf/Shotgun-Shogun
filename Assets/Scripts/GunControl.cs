e4862a80a2d45253ac9aacb9b00d70137abbefcc
Gadnalf <philip.cai.mao@hotmail.ca>
2019-09-18 14:59:37 -0400

    Fixed the aiming almost, except for direction and jitters

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -28,13 +28,13 @@ public class GunControl : MonoBehaviour
     {
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         //Debug.Log("squeak: " + mousePos.ToString());
-        //Debug.Log("shoulder: " + leftShoulder.position);
+        //Debug.Log("shrug: " + leftShoulder.position);
         //Debug.Log("elbow noise: " + leftElbow.position);
         double aimAngle = FindAimAngle(leftShoulder.position, leftElbow.position, mousePos) * (180 / Math.PI);
         Debug.Log("angle time: " + aimAngle);
         Vector3 rotate = new Vector3(0, 0, (float)aimAngle);
         //rightShoulder.Rotate(rotate);
-        //leftShoulder.Rotate(rotate);
+        leftShoulder.Rotate(rotate);
     }
 
     private double Dist2D(Vector3 a, Vector3 b)
@@ -48,17 +48,35 @@ public class GunControl : MonoBehaviour
         Vector3 mid = new Vector3((pivot.x + target.x)/2, (pivot.y + target.y) / 2, 0);
         //Debug.Log("full distance: " + Dist2D(pivot, target));
         //Debug.Log("mid distance: " + Dist2D(pivot, mid));
-        int numIntersects = FindIntersectOfCircles(pivot, Dist2D(pivot, elbow), mid, Dist2D(pivot, mid), out intersect1, out intersect2);
+        Vector3 pivot1 = pivot;
+        double radius1 = Dist2D(pivot, elbow);
+        Vector3 pivot2 = mid;
+        double radius2 = Dist2D(pivot, mid);
+        int numIntersects = FindIntersectOfCircles(pivot1, radius1, pivot2, radius2, out intersect1, out intersect2);
+
         double lengthToMidpoint;
+        Vector3 newElbow;
         if (numIntersects == 2)
         {
             //Debug.Log("jabroni time: " + intersect1 + intersect2);
-            lengthToMidpoint = Math.Min(Dist2D(elbow, intersect1), Dist2D(elbow, intersect2)) / 2;
+            double dist1 = Dist2D(elbow, intersect1);
+            double dist2 = Dist2D(elbow, intersect2);
+            if (dist1 <= dist2)
+            {
+                lengthToMidpoint = dist1 / 2;
+                newElbow = intersect1;
+            }
+            else
+            {
+                lengthToMidpoint = dist2 / 2;
+                newElbow = intersect2;
+            }
         }
         else if (numIntersects == 1)
         {
             //Debug.Log("merry christmas: " + intersect1);
             lengthToMidpoint = Dist2D(elbow, intersect1) / 2;
+            newElbow = intersect1;
         }
         else
         {
@@ -70,8 +88,14 @@ public class GunControl : MonoBehaviour
             Debug.Log("wait, what the fuck?");
             return 0;
         }
-        Debug.Log("We gotta angle here, between " + pivot + " and " + elbow + ". Length to mid is " + lengthToMidpoint + " and dist is " + Dist2D(pivot, elbow));
-        return Math.Acos(lengthToMidpoint / Dist2D(pivot, elbow)) * 2;
+        //Debug.Log("We gotta angle here, between " + pivot + " and " + intersect1 + " or " + intersect2 + ". Length to mid is " + lengthToMidpoint);
+        double returnAngle = ((90 * Math.PI / 180) - Math.Acos(lengthToMidpoint / Dist2D(pivot, elbow))) * 2;
+        if (newElbow.y < elbow.y)
+        {
+            return returnAngle * -1;
+        }
+        return returnAngle;
+        
     }
 
     private int FindIntersectOfCircles(Vector3 center1, double radius1, Vector3 center2, double radius2, out Vector3 intersect1, out Vector3 intersect2)
@@ -81,9 +105,9 @@ public class GunControl : MonoBehaviour
         if (dist > radius1 + radius2 | dist < Math.Abs(radius1 - radius2) | dist == 0) // There's some fuckery afoot
         {
             Debug.Log("there's fuckery afoot");
-            Debug.Log("too far?" + (dist > radius1 + radius2));
-            Debug.Log("one circle in the other somehow?" + (dist < Math.Abs(radius1 - radius2)));
-            Debug.Log("same pivot?" + (dist == 0));
+            Debug.Log("too far? " + (dist > radius1 + radius2));
+            Debug.Log("one circle in the other somehow? " + (dist < Math.Abs(radius1 - radius2)));
+            Debug.Log("same pivot? " + (dist == 0));
             intersect1 = center1;
             intersect2 = center1;
             return 0;
@@ -101,16 +125,14 @@ public class GunControl : MonoBehaviour
             double a = (Math.Pow(radius1, 2) - Math.Pow(radius2, 2) + Math.Pow(dist, 2)) / (2 * dist);
             double h = Math.Sqrt(Math.Pow(radius1, 2) - Math.Pow(a, 2));
 
-            Vector3 direction = (center1 - center2).normalized;
-            direction.Scale(new Vector3((float)a, (float)a, 0));
+            Vector3 direction = new Vector3 ((float) ((center2.x - center1.x) / dist), (float) ((center2.y - center1.y) / dist), 0);
+            Vector3 horizontal = new Vector3(direction.x * (float) a, direction.y * (float) a, 0);
 
-            Vector3 vert = (center1 - center2).normalized;
-            Vector3 avert = vert;
-            vert.Scale(new Vector3((float)h, (float)-h, 0));
-            avert.Scale(new Vector3((float)-h, (float)h, 0));
+            Vector3 vert = new Vector3(direction.y * (float)h, -1 * direction.x * (float)h, 0);
+            Vector3 avert = new Vector3(-1 * direction.y * (float)h, direction.x * (float)h, 0);
 
-            intersect1 = center1 + direction + vert;
-            intersect2 = center1 + direction + avert;
+            intersect1 = center1 + horizontal + vert;
+            intersect2 = center1 + horizontal + avert;
             return 2;
         }
 
